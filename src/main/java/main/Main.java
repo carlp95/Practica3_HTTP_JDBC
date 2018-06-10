5c5f71a2173e37bb4ccb546b1bc73526088c9a84
Carlos <carlosperezurena@gmail.com>
2018-06-09 21:08:36 -0400

    Formulario de crear usuarios en proceso

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,9 @@
 package main;
 
 import BD.Dao;
+import Estructura.Usuario;
 import freemarker.template.Configuration;
+import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 
 import java.util.HashMap;
@@ -17,10 +19,27 @@ public class Main {
 
         Dao conexionBD = new Dao();
 
-        /*get("/createUser", (request,response) ->{
+        get("/createUser", (request,response) ->{
             Map<String, Object> atributos = new HashMap<>();
+            atributos.put("titulo","Crear Usuario");
+            return new ModelAndView(atributos,"createUser.ftl");
+        },freemarkerEngine);
 
+        post("/createUser", (request, response) ->{
+            Usuario user = new Usuario();
+            user.setUsername(request.queryParams("username"));
+            user.setContrasena(request.queryParams("contrasena"));
+            if(request.queryParams("administrador").equals("true")){
+                user.setAdministrador(true);
+                user.setAutor(true);
+            }else if(request.queryParams("autor").equals("true")){
+                user.setAutor(true);
+                user.setAdministrador(false);
+            }
+            conexionBD.insertarUsuario(user);
+
+            response.redirect("/");
             return null;
-        },freemarkerEngine);*/
+        }, freemarkerEngine);
     }
 }
