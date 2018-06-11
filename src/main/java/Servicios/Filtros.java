1fcb0a14257608c19805a7fca352de37ea10dde2
Carlos <carlosperezurena@gmail.com>
2018-06-11 05:53:55 -0400

    Login terminado

--- src/main/java/Servicios/Filtros.java
+++ src/main/java/Servicios/Filtros.java
@@ -1,5 +1,22 @@
 package Servicios;
 
+import Estructura.Usuario;
+
+import static spark.Spark.before;
+import static spark.Spark.halt;
+
 public class Filtros {
-    
+
+    public Filtros() {
+    }
+
+    public void aplicarFiltros(){
+        before("/confirmLogin/*",(request, response) -> {
+            Usuario usuario = request.session(true).attribute("usuario");
+            if(usuario==null){
+                //parada del request, enviando un codigo.
+                halt(401, "No se ha podido verificar sus credenciales");
+            }
+        });
+    }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,24 @@
 package main;
 
 import BD.Dao;
+import Estructura.Articulo;
+import Estructura.Comentario;
 import Estructura.Usuario;
 import freemarker.template.Configuration;
 import org.jasypt.util.password.BasicPasswordEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
 import spark.ModelAndView;
+import spark.Session;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Main {
-
     public static void main(String[] args) {
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_26);
         configuration.setClassForTemplateLoading(Main.class, "/templates");
@@ -28,6 +33,78 @@ public class Main {
 
             return new ModelAndView(model, "index.ftl");
         }, freemarkerEngine);
+
+        get("/login",(request, response) ->{
+            Map<String, Object> atributos = new HashMap<>();
+            atributos.put("titulo", "Login");
+            return new ModelAndView(atributos,"login.ftl");
+        },freemarkerEngine);
+
+        post("/confirmLogin", (request,response) ->{
+            String usuario = request.queryParams("username");
+            String contrasena = request.queryParams("contrasena");
+            BasicPasswordEncryptor encryptor = new BasicPasswordEncryptor();
+            String encriptedPass = encryptor.encryptPassword(contrasena);
+
+            if(request.queryParams("recordar").equals("on")){
+                    Session session = request.session(true);
+                    List<Usuario> usuarios = conexionBD.getUsuarios();
+
+                    for(Usuario user : usuarios){
+                        boolean verificacion = encryptor.checkPassword(contrasena, user.getContrasena());
+                        if(request.queryParams("username").equalsIgnoreCase(user.getUsername()) && verificacion){
+                            BasicTextEncryptor encriptadorTexto = new BasicTextEncryptor();
+                            response.cookie("/","Sesion", encriptadorTexto.encrypt(session.id()), 604800, true);
+                            session.attribute("usuario", user);
+                        }else{
+                            halt(401,"Credenciales invalidas...");
+                        }
+                    }
+                    response.redirect("/");
+                }else {
+
+                    Session session = request.session(true);
+                    List<Usuario> usuarios = conexionBD.getUsuarios();
+
+                    for(Usuario user : usuarios){
+                        boolean verificacion = encryptor.checkPassword(contrasena, user.getContrasena());
+                        if(request.queryParams("username").equalsIgnoreCase(user.getUsername()) && verificacion){
+                            session.attribute("usuario", user);
+                        }else{
+                            halt(401,"Credenciales invalidas...");
+                        }
+                    }
+                    response.redirect("/");
+                }
+            return null;
+        },freemarkerEngine);
+
+        get("/show/:id",(request, response) ->{
+            Map<String, Object> atributos = new HashMap<>();
+            List<Articulo> articulos = conexionBD.getArticulos();
+            for(Articulo art : articulos){
+                if(Long.parseLong(request.params("id")) == art.getId()){
+                    atributos.put("titulo",art.getTitulo());
+                    atributos.put("articulo",art);
+                }
+            }
+
+            return new ModelAndView(atributos,"showArticle.ftl");
+        },freemarkerEngine);
+
+        post("/createComment",(request,response)->{
+            Comentario comentario = new Comentario();
+            Usuario user = request.session(true).attribute("usuario");
+
+            for(Articulo art : conexionBD.getArticulos()){
+                if(Long.parseLong(request.queryParams("id")) == art.getId()){
+                    comentario.setComentario(request.queryParams("comentario"));
+                    comentario.setAutor(user);
+                    comentario.setArticulo(art);
+                }
+            }
+            return null;
+        },freemarkerEngine);
 
         get("/createUser", (request,response) ->{
             Map<String, Object> atributos = new HashMap<>();
@@ -54,5 +131,4 @@ public class Main {
 
         }, freemarkerEngine);
     }
-
 }
